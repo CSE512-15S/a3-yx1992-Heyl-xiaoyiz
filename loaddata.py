2a9456435cfbd0fbd41faa4cfe936dd9fe5565d6
yangxinuw <yangxin199207@gmail.com>
2015-05-02 23:44:05 -0700

    data processing

--- loaddata.py
+++ loaddata.py
@@ -22,10 +22,15 @@ with open("./WDI_csv/WDI_Data.csv",'r') as csvfile:
             head=row
         else:
             if row[3]==GDPstring:
-                line=row[0]
+                line=row[0]+","+row[1]
                 for j in range(size(row)-4):
-                    line=line+","+row[j+4]
+                    entry=row[j+4]
+                    if entry=="":
+                        entry="0"
+                    line=line+","+entry
                 line=line+"\n"
+                print row
+                print line
                 fout.write(line)
 fout.close()
 print minj
